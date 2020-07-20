db5736156f77e1e6d8a1e994b5cf8d2f71be660b
Ludwing Rivera Amador <ludwingnidoo@192.168.0.3>
2020-07-19 22:58:16 -0500

    Pages place and place-detail, config routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'places',
     pathMatch: 'full'
+  },
+  {
+    path: 'places',
+    children: [
+      {
+        path: "",
+        loadChildren: () => import('./places/places.module').then(m => m.PlacesPageModule)
+      },
+      {
+        path: ":placeId",
+        loadChildren: () => import('./places/place-detail/place-detail.module').then(m => m.PlaceDetailPageModule)
+      }
+    ]
   },
 ];
 
